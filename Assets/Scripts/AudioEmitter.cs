a88e91fca135328e59b6a9cac2bd161b0d004c4f
glitch-cg <glitch.cg.0314@gmail.com>
2024-09-23 21:56:34 +0900

    Solved trigger event problem for audio emission
    Solved audio doubling emission problem for inward, upward, downward view direction cases

--- Assets/Scripts/AudioEmitter.cs
+++ Assets/Scripts/AudioEmitter.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class AudioEmitter : MonoBehaviour
 {
+    public GameObject directionManageObj = null;
+    private DirectionController dc = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dc = directionManageObj.GetComponent<DirectionController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(dc.currentDirection == DirectionController.DirectionType.Outward)
+        {
+            GetComponent<BoxCollider>().enabled = true;
+        }
+        else GetComponent<BoxCollider>().enabled = false;
     }
 
     private void OnTriggerEnter(Collider other) 
